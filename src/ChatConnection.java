31f2a08e4a0c94e04cad74fec9e6ead081502300
ClintFowler <jedd1876@gmail.com>
2015-06-11 21:06:31 -0600

    Initial Structure

---
+++ src/ChatConnection.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Clinton on 6/11/2015.
+ */
+public class ChatConnection implements Runnable
+{
+    public void ChatConnection(String serverip, int portnum)
+    {
+
+    }
+
+    @Override
+    public void run()
+    {
+
+    }
+}
---
+++ src/LAB4_5.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Clinton on 6/11/2015.
+ */
+public class LAB4_5
+{
+
+}
