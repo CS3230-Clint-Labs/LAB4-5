d64943fb639674b072ff998201c13bf31b64240a
Tyler Cazier <caziertyler@gmail.com>
2015-06-14 12:14:26 -0600

    Added JMenuBar with menus to allow user to input server IP and connection port number.

--- src/LAB4_5/ChatServerListener.java
+++ src/LAB4_5/ChatServerListener.java
@@ -54,7 +54,7 @@ public class ChatServerListener implements Runnable
                         }
                     } catch (NullPointerException e)
                     {
-
+                        e.printStackTrace();
                     }
                 }
             }
